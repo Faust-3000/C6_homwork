0c80e2d08b690e39ab8e15e71d83bc91941c8f35
Faust3000 <mephistopheles3000@gmail.com>
2022-10-23 16:42:17 +0400

    решил 2ю задачу

--- Program.cs
+++ Program.cs
@@ -3,7 +3,7 @@
 0, 7, 8, -2, -2 -> 2
 1, -7, 567, 89, 223-> 4 */
 
-Console.Write("Введите количество чисел для ввода ");
+/*Console.Write("Введите количество чисел для ввода ");
 int n = Convert.ToInt32(Console.ReadLine()!);
 int[] array = new int[n];
 
@@ -15,4 +15,25 @@ int count = 0;
 for(int i = 0; i < n; i++){
     if(array[i] > 0) count++;
 }
-Console.WriteLine($"Чисел больше 0 введенных пользователем равно: {count}");
+Console.WriteLine($"Чисел больше 0 введенных пользователем равно: {count}"); */
+
+/*Напишите программу, которая найдёт точку пересечения двух прямых, 
+заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 
+задаются пользователем. */
+
+/*Console.Write("значение b1 = ");
+double a = Convert.ToDouble(Console.ReadLine()!);
+Console.Write("значение k1 = ");
+double b = Convert.ToDouble(Console.ReadLine()!);
+Console.Write("значение b2 = ");
+double c = Convert.ToDouble(Console.ReadLine()!);
+Console.Write("значение k2 = ");
+double d = Convert.ToDouble(Console.ReadLine()!);
+
+if(b != d){
+double x = ((a - c) / (b - d));
+double y = ((d * a) - (b * c) / (d - b));
+Console.WriteLine($"Координаты точки пересечения прямых: ({Math.Round(x, 1)}; {Math.Round(y, 1)})");
+}
+else 
+Console.WriteLine("Прямые параллельны");*/
